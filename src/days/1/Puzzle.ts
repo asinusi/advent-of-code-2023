72f1be4f1c0626a3e9d4c26b565baaee60fd4e6e
Abdul Sinusi <asinusi@outlook.com>
2022-12-04 12:09:51 +0200

    First day challenge

--- src/days/1/Puzzle.ts
+++ src/days/1/Puzzle.ts
@@ -1,11 +1,37 @@
 import Puzzle from '../../types/AbstractPuzzle';
+import readFile from '../../utils/readFile';
 
 export default class ConcretePuzzle extends Puzzle {
+  
+  private getTotalCalories() {
+    readFile('src/days/1/input.txt').then((input) => this.setInput(input));
+    
+    const calories = this.input.split('\r\n');
+    const totalCalories: number[] = [];
+    let total = 0;
+    for (const calorie of calories) {
+      if (calorie === '') {
+        // Create new array
+        totalCalories.push(total);
+        total = 0;
+      } else {
+        total += parseInt(calorie);
+      }
+    }
+  
+    totalCalories.push(total);
+
+    return totalCalories;
+  }
   public solveFirst(): string {
-    return 'day 1 solution 1';
+    const totalCalories = this.getTotalCalories();
+    
+    return Math.max(...totalCalories).toString();
   }
   public solveSecond(): string {
-    return 'day 1 solution 2';
+    const totalCalories = this.getTotalCalories().sort((a, b) => b - a);
+
+    return (totalCalories[0] + totalCalories[1] + totalCalories[2]).toString();
   }
 
   public getFirstExpectedResult(): string {
