4a0b7cc654e4aa79a4f6ad4973e3819fa0406369
Abdul Sinusi <asinusi@outlook.com>
2022-12-06 12:16:52 +0200

    Day 6 challenge

---
+++ src/days/6/Puzzle.ts
@@ -0,0 +1,50 @@
+import Puzzle from '../../types/AbstractPuzzle';
+import readFile from '../../utils/readFile';
+
+export default class ConcretePuzzle extends Puzzle {
+  public solveFirst(): string {
+    readFile('./src/days/6/input.txt').then((input) => this.setInput(input));
+
+    const stream = this.input;
+    let marker: number;
+    for (let i = 0; i < stream.length; i++) {
+      // Check for marker based on current index plus 4
+      const letters = new Set(stream.slice(i, i + 4).split(''));
+
+      if (letters.size === 4) {
+        marker = i + 4;
+        break;
+      }
+    }
+    // Read the stream in 4 character chunks
+    return marker.toString();
+  }
+
+  public getFirstExpectedResult(): string {
+    // RETURN EXPECTED SOLUTION FOR TEST 1;
+    return 'day 1 solution 1';
+  }
+
+  public solveSecond(): string {
+    readFile('./src/days/6/input.txt').then((input) => this.setInput(input));
+
+    const stream = this.input;
+    let marker: number;
+    for (let i = 0; i < stream.length; i++) {
+      // Check for marker based on current index plus 14
+      const letters = new Set(stream.slice(i, i + 14).split(''));
+
+      if (letters.size === 14) {
+        marker = i + 14;
+        break;
+      }
+    }
+    // Read the stream in 4 character chunks
+    return marker.toString();
+  }
+
+  public getSecondExpectedResult(): string {
+    // RETURN EXPECTED SOLUTION FOR TEST 2;
+    return 'day 1 solution 2';
+  }
+}
